e212609b7e0c564f8ce637cf064a1ef805bab26d
Reece Kidd <reecekidd95@gmail.com>
2022-02-28 09:59:11 +0000

    refactor: added account to the header

--- accountability/components/Header/Header.tsx
+++ accountability/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useWeb3React } from "@web3-react/core";
 import Link from "next/link";
 import { FC } from "react";
 import { Button, Menu } from "semantic-ui-react";
@@ -5,12 +6,14 @@ import { Button, Menu } from "semantic-ui-react";
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
+  const { account } = useWeb3React();
   return (
     <Menu style={{ marginTop: "10px" }}>
       <Link href={"/"} passHref>
         <Menu.Item>Crypto accountability</Menu.Item>
       </Link>
       <Menu.Menu position="right">
+        <Menu.Item>{account}</Menu.Item>
         <Link href={"/contracts"} passHref={true}>
           <Menu.Item>Contracts</Menu.Item>
         </Link>
