14eccaea634f595beae525478056b35a37957512
Reece Kidd <reecekidd95@gmail.com>
2022-03-14 10:22:46 +0000

    refactor: add modal to say you need to connect wallet

--- accountability/components/Header/Header.tsx
+++ accountability/components/Header/Header.tsx
@@ -1,29 +1,53 @@
 import { useWeb3React } from "@web3-react/core";
 import Link from "next/link";
-import { FC } from "react";
-import { Button, Menu } from "semantic-ui-react";
+import { FC, useState } from "react";
+import { Button, Icon, Menu, Modal } from "semantic-ui-react";
 
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
   const { account } = useWeb3React();
+  const [openModal, setOpenModal] = useState(false);
   return (
-    <Menu style={{ marginTop: "10px" }}>
-      <Link href={"/"} passHref>
-        <Menu.Item>Crypto accountability</Menu.Item>
-      </Link>
-      <Menu.Menu position="right">
-        <Menu.Item>{account}</Menu.Item>
-        <Link href={"/contracts"} passHref={true}>
-          <Menu.Item>Contracts</Menu.Item>
+    <>
+      <Menu style={{ marginTop: "10px" }}>
+        <Link href={"/"} passHref>
+          <Menu.Item>Crypto accountability</Menu.Item>
         </Link>
-        <Link href="/contracts/new" passHref={true}>
-          <Menu.Item link={true}>
-            <Button primary icon="plus" />
-          </Menu.Item>
-        </Link>
-      </Menu.Menu>
-    </Menu>
+        <Menu.Menu position="right">
+          {account ? (
+            <Menu.Item>{account}</Menu.Item>
+          ) : (
+            <Menu.Item>
+              <Button
+                color="red"
+                content={"Connect wallet"}
+                onClick={() => setOpenModal(!openModal)}
+              />
+            </Menu.Item>
+          )}
+
+          {account && (
+            <>
+              <Link href={"/contracts"} passHref={true}>
+                <Menu.Item>Contracts</Menu.Item>
+              </Link>
+              <Link href="/contracts/new" passHref={true}>
+                <Menu.Item link={true}>
+                  <Button primary icon="plus" />
+                </Menu.Item>
+              </Link>
+            </>
+          )}
+        </Menu.Menu>
+      </Menu>
+      <Modal
+        closeIcon
+        header="You need to connect your Metamask wallet"
+        onClose={() => setOpenModal(false)}
+        open={openModal}
+      />
+    </>
   );
 };
 
